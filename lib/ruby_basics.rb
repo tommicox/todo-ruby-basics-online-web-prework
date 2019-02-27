3a92ffd33cef5bc260aab812013ea68b502138d3
Tommi Cox <tommicox@gmail.com>
2019-02-27 00:06:37 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,25 +1,31 @@
 def division(num1, num2)
- 
+ puts num1, num2
+ return num1, num2
 end
 
 def assign_variable(value)
+  puts value
+  return value
+end
+
+def argue(phrase)
+  puts phrase
+  return phrase
+  end
   
+def greeting (phrase = "x",name = "Bob")
+  puts (phrase, name)
+  return (phrase, name)
 end
 
-def argue
-  
+def return_a_value(phrase = "nice")
+  puts phrase
+  return phrase
 end
 
-def greeting
-
-end
-
-def return_a_value
-
-end
-
-def last_evaluated_value
-
+def last_evaluated_value(phrase)
+  puts phrase
+  return phrase
 end
 
 def pizza_party
